d7749fedf347f4b93417fef3e02a2b9611fd1560
benj <bortotbenj@hotmail.com>
2019-06-04 23:36:59 +0200

    smart factory

--- src/app/dynamicForms/question.service.ts
+++ src/app/dynamicForms/question.service.ts
@@ -4,22 +4,32 @@ import { QuestionBase }     from './question-base';
 import { TextboxQuestion }  from './question-textbox';
 import { DropdownQuestion } from './question-dropdown';
 import { StarratedQuestion } from './question-starrated';
+import * as sessionData from '../sessions/data/sessions.json';
+
 
 @Injectable()
 export class QuestionService {
- 
-  questionFactory(questionInput) : QuestionBase<any> {
+  
+  
+
+  questionFactory(questionAnswerInput, globalQuestionType, globalAnswerList, globalDefaultValue, keyOrder) : QuestionBase<any> {
     let question : QuestionBase<any>;
-    switch(questionInput.questionType){
+    let questionType = (globalQuestionType)? globalQuestionType : questionAnswerInput.questionType;
+    let answerList = (globalAnswerList)? globalAnswerList : questionAnswerInput.answerList;
+    let questionKey = (keyOrder)? keyOrder : questionAnswerInput.key;
+    let questionOrder = (keyOrder)? keyOrder : questionAnswerInput.order;
+    let value = (globalDefaultValue)? globalDefaultValue : questionAnswerInput.value;
+    console.log('answerList',answerList);
+    switch(questionType){
       case "starScored":
           return new StarratedQuestion({
-            key: questionInput.key,
-            label: questionInput.label,
-            value: questionInput.value,
-            required:  questionInput.required,
-            order:  questionInput.order,
-            max:questionInput.max,
-            options: questionInput.options
+            keyOrder: questionKey,
+            label: questionAnswerInput.label,
+            value: value,
+            required: questionAnswerInput.required,
+            order: questionOrder,
+            max: answerList.length,
+            options: answerList
           })
     }
   }
@@ -27,7 +37,15 @@ export class QuestionService {
   // TODO: get from a remote source of question metadata
   // TODO: make asynchronous
   getQuestions() {
-    let questions: QuestionBase<any>[] = [
+    let questions: QuestionBase<any>[] = [];
+    console.log(sessionData.session);
+    let globalQuestionType = sessionData.session.sessionEval.questionAnswers.globalQuestionType;
+    let globalAnswerList = sessionData.session.sessionEval.questionAnswers.globalAnswerList;
+    let globalDefaultValue = sessionData.session.sessionEval.questionAnswers.globalDefaultValue;
+    sessionData.session.sessionEval.questionAnswers.questionList.forEach((quest, key) => {
+      questions.push(this.questionFactory(quest,globalQuestionType, globalAnswerList, globalDefaultValue, key ));
+      console.log(this.questionFactory(quest,globalQuestionType, globalAnswerList, globalDefaultValue, key ));
+    });
       // new DropdownQuestion({
       //   key: 'brave',
       //   label: 'Bravery Rating',
@@ -39,36 +57,36 @@ export class QuestionService {
       //   ],
       //   order: 3
       // }),
-      new StarratedQuestion({
-        key: 'question1',
-        label: 'Avez vous déja utilisé Jira?',
-        value: 1,
-        required: true,
-        order: 3,
-        max:4,
-        options: [
-              'jamais',
-              'deux trois fois',
-              'tous les jours',
-              'je suis Jira'
-            ]
-      }),
+    //   new StarratedQuestion({
+    //     key: 'question1',
+    //     label: 'Avez vous déja utilisé Jira?',
+    //     value: 1,
+    //     required: true,
+    //     order: 3,
+    //     max:4,
+    //     options: [
+    //           'jamais',
+    //           'deux trois fois',
+    //           'tous les jours',
+    //           'je suis Jira'
+    //         ]
+    //   }),
 
-      new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
-        required: true,
-        order: 1
-      }),
+    //   new TextboxQuestion({
+    //     key: 'firstName',
+    //     label: 'First name',
+    //     value: 'Bombasto',
+    //     required: true,
+    //     order: 1
+    //   }),
  
-      new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
-        order: 2
-      })
-    ];
+    //   new TextboxQuestion({
+    //     key: 'emailAddress',
+    //     label: 'Email',
+    //     type: 'email',
+    //     order: 2
+    //   })
+    // ];
  
     return questions.sort((a, b) => a.order - b.order);
   }
