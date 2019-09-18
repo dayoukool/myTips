4e9b96ebe968bedec6e4234f9b459b791ed10d07
Antoine <ant28121@hotmail.fr>
2019-09-18 15:41:08 +0200

    modal

--- src/app/sessions/card-sessions/card-sessions.component.ts
+++ src/app/sessions/card-sessions/card-sessions.component.ts
@@ -7,6 +7,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 @Component({
   selector: 'SessionDetail',
   templateUrl: './session-detail.html',
+  styleUrls: ['./card-sessions.component.sass']
 })
 export class SessionDetail {
 
