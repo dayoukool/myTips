318a7cd3ee69528b39e6e0aaf7b9a788f08338b2
Antoine <antoinedugue@sfr.fr>
2019-09-27 16:04:55 +0200

    Nettoyage

--- src/app/Pipe/filter-date-pipe.pipe.ts
+++ src/app/Pipe/filter-date-pipe.pipe.ts
@@ -13,35 +13,24 @@ export class FilterDatePipePipe implements PipeTransform {
   public maxValue: Date;
   public minValueNumber: Number;
   public maxValueNumber: Number;
-  
+
 
   constructor() {
 
   }
   transform(Slides: Session[], minValue: Date, maxValue: Date): any {
-    console.log('Slides :');
-    console.log(Slides);
-    console.log('minValue :');
-    console.log(minValue);
-    console.log(maxValue);
+    
     this.maxValueNumber = Date.parse(maxValue.toString());
     this.minValueNumber = Date.parse(minValue.toString());
     if (!Slides) {
-      console.log('on filtre pas');
       return Slides;
     }
-    console.log('on filtre');
-    console.log('dates :');
     Slides = Slides.filter((el) => {
-      console.log(el.date);
-      console.log(new Date(el.date));
       el.date = new Date(el.date);
-      console.log(Date.parse(el.date.toString()));
-      console.log(Date.parse(el.date.toString()) <= this.maxValueNumber && Date.parse(el.date.toString()) >= this.minValueNumber);
       return Date.parse(el.date.toString()) <= this.maxValueNumber && Date.parse(el.date.toString()) >= this.minValueNumber;
     }
     );
-    console.log(Slides);
+    
     return Slides;
   }
 
--- src/app/demande-sessions/demande-sessions.component.ts
+++ src/app/demande-sessions/demande-sessions.component.ts
@@ -16,22 +16,22 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { Date: new Date('December 11, 2020 11:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 1, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 4, 2020 12:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('October 15, 2020 08:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('December 22, 2020 09:30:00'), Auteur: 'Jhon', Sujet: 'Jira', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 13, 2020 16:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 1, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('October 6, 2020 17:30:00'), Auteur: 'Eric', Sujet: 'Jira', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('December 18, 2020 18:30:00'), Auteur: 'Doe', Sujet: 'Confluence', Module: 1, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 28, 2020 14:30:00'), Auteur: 'Jacques', Sujet: 'Jira', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('October 26, 2020 13:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('December 30, 2020 17:30:00'), Auteur: 'Nicolas', Sujet: 'Confluence', Module: 1, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 13, 2020 15:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('October 12, 2020 10:30:00'), Auteur: 'François', Sujet: 'Jira', Module: 1, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 6, 2020 9:30:00'), Auteur: 'Doe', Sujet: 'Confluence', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('December 17, 2020 8:30:00'), Auteur: 'Emmanuel', Sujet: 'Jira', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('November 9, 2020 15:30:00'), Auteur: 'Valérie', Sujet: 'Confluence', Module: 3, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
-  { Date: new Date('December 7, 2020 14:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
+  { Date: new Date('December 11, 2020 11:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 1, DateVoulu: new Date('December 16, 2020 10:30:00'), Action: 'coucou' },
+  { Date: new Date('November 4, 2020 12:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date('December 1, 2020 09:30:00'), Action: 'coucou' },
+  { Date: new Date('October 15, 2020 08:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 3, DateVoulu: new Date('November 16, 2020 15:30:00'), Action: 'coucou' },
+  { Date: new Date('December 22, 2020 09:30:00'), Auteur: 'Jhon', Sujet: 'Jira', Module: 2, DateVoulu: new Date('January 5, 2021 14:30:00'), Action: 'coucou' },
+  { Date: new Date('November 13, 2020 16:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 1, DateVoulu: new Date('December 4, 2020 10:30:00'), Action: 'coucou' },
+  { Date: new Date('October 6, 2020 17:30:00'), Auteur: 'Eric', Sujet: 'Jira', Module: 3, DateVoulu: new Date('November 18, 2020 08:30:00'), Action: 'coucou' },
+  { Date: new Date('December 18, 2020 18:30:00'), Auteur: 'Doe', Sujet: 'Confluence', Module: 1, DateVoulu: new Date('January 17, 2020 08:30:00'), Action: 'coucou' },
+  { Date: new Date('November 28, 2020 14:30:00'), Auteur: 'Jacques', Sujet: 'Jira', Module: 3, DateVoulu: new Date('December 18, 2020 11:30:00'), Action: 'coucou' },
+  { Date: new Date('October 26, 2020 13:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date('November 19, 2020 11:30:00'), Action: 'coucou' },
+  { Date: new Date('December 30, 2020 17:30:00'), Auteur: 'Nicolas', Sujet: 'Confluence', Module: 1, DateVoulu: new Date('January 5, 2021 07:30:00'), Action: 'coucou' },
+  { Date: new Date('November 13, 2020 15:30:00'), Auteur: 'Doe', Sujet: 'Jira', Module: 3, DateVoulu: new Date('January 8, 2021 07:30:00'), Action: 'coucou' },
+  { Date: new Date('October 12, 2020 10:30:00'), Auteur: 'François', Sujet: 'Jira', Module: 1, DateVoulu: new Date('November 9, 2020 07:30:00'), Action: 'coucou' },
+  { Date: new Date('November 6, 2020 9:30:00'), Auteur: 'Doe', Sujet: 'Confluence', Module: 3, DateVoulu: new Date('November 30, 2020 07:30:00'), Action: 'coucou' },
+  { Date: new Date('December 17, 2020 8:30:00'), Auteur: 'Emmanuel', Sujet: 'Jira', Module: 2, DateVoulu: new Date('January 29, 2021 15:30:00'), Action: 'coucou' },
+  { Date: new Date('November 9, 2020 15:30:00'), Auteur: 'Valérie', Sujet: 'Confluence', Module: 3, DateVoulu: new Date('December 03, 2020 15:30:00'), Action: 'coucou' },
+  { Date: new Date('December 7, 2020 14:30:00'), Auteur: 'Doe', Sujet: 'Test auto', Module: 2, DateVoulu: new Date('December 18, 2020 15:30:00'), Action: 'coucou' },
 ];
 
 const frenchRangeLabel = (page: number, pageSize: number, length: number) => {
