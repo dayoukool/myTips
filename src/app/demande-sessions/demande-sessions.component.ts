8c10fca1de9da0e0235a4ad5821c368e7f8ff3d1
Antoine <antoinedugue@sfr.fr>
2019-09-27 11:49:31 +0200

    pagination FR

--- src/app/demande-sessions/demande-sessions.component.ts
+++ src/app/demande-sessions/demande-sessions.component.ts
@@ -34,11 +34,28 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { Date: new Date(25 / 12 / 2020), Auteur: 'Doe', Sujet: 'Jira', Module: 2, DateVoulu: new Date(13 / 5 / 2021), Action: 'coucou' },
 ];
 
+const frenchRangeLabel = (page: number, pageSize: number, length: number) => {
+  if (length == 0 || pageSize == 0) { return `0 van ${length}`; }
+
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize;
+
+  // If the start index exceeds the list length, do not try and fix the end index to the end.
+  const endIndex = startIndex < length ?
+    Math.min(startIndex + pageSize, length) :
+    startIndex + pageSize;
+
+  return `${startIndex + 1} - ${endIndex} sur ${length}`;
+}
+
 @Component({
   selector: 'app-demande-sessions',
   templateUrl: './demande-sessions.component.html',
   styleUrls: ['./demande-sessions.component.sass']
 })
+
+
 
 export class DemandeSessionsComponent implements OnInit {
 
@@ -52,7 +69,15 @@ export class DemandeSessionsComponent implements OnInit {
   ngOnInit() {
     this.demandeDeSessionsArray.paginator = this.paginator;
     this.demandeDeSessionsArray.sort = this.sort;
+    this.demandeDeSessionsArray.paginator._intl.itemsPerPageLabel = 'Résultats par pages';
+    this.demandeDeSessionsArray.paginator._intl.nextPageLabel = 'Page suivante';
+    this.demandeDeSessionsArray.paginator._intl.previousPageLabel = 'Page précédente';
+    this.demandeDeSessionsArray.paginator._intl.firstPageLabel = 'Première page';
+    this.demandeDeSessionsArray.paginator._intl.lastPageLabel = 'Dernière page';
+    this.demandeDeSessionsArray.paginator._intl.getRangeLabel = frenchRangeLabel;
   }
+
+
 
   applyFilter(filterValue: string) {
     this.demandeDeSessionsArray.filter = filterValue.trim().toLowerCase();
